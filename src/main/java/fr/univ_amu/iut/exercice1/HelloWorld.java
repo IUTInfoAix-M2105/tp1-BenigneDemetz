b7b86b1dfd6e73d6b90243c1f16d06ec36d9390c
BenigneDe <benigne.demetz@etu.univ-amu.fr>
2021-04-21 15:29:08 +0200

    Exercice HelloWorld comprenend les conditions alice, bob, chaine vide et chaine null et implementation de resultat de 1 dans fizzbuzzer

--- src/main/java/fr/univ_amu/iut/exercice1/HelloWorld.java
+++ src/main/java/fr/univ_amu/iut/exercice1/HelloWorld.java
@@ -2,6 +2,13 @@ package fr.univ_amu.iut.exercice1;
 
 public class HelloWorld {
     public static String hello(String s) {
-        throw new RuntimeException("Not yet implemented !");
+        if (s == null || s.isEmpty())
+        {
+            return "Hello, World!";
+        }
+        else
+        {
+            return "Hello, " + s + "!";
+        }
     }
 }
