425384f1f21b805a403d786731bf378734f7c179
BenigneDe <benigne.demetz@etu.univ-amu.fr>
2021-04-21 15:31:51 +0200

    implementation de resultat de 2 dans fizzbuzzer

--- src/main/java/fr/univ_amu/iut/exercice2/FizzBuzzer.java
+++ src/main/java/fr/univ_amu/iut/exercice2/FizzBuzzer.java
@@ -3,7 +3,11 @@ package fr.univ_amu.iut.exercice2;
 public class FizzBuzzer {
 
     public String computeString(int i) {
-        return "1";
+        if (i == 1)
+            return "1";
+        if (i == 2)
+            return "2";
+        throw new RuntimeException("Not yet implemented !");
     }
 
     public String[] computeList(int i) {
