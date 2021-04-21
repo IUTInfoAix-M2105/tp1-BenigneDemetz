8447f5346479560589a09eabea6029017cf89f52
BenigneDe <benigne.demetz@etu.univ-amu.fr>
2021-04-21 15:36:10 +0200

    refactor de FizzBuzzer

--- src/main/java/fr/univ_amu/iut/exercice2/FizzBuzzer.java
+++ src/main/java/fr/univ_amu/iut/exercice2/FizzBuzzer.java
@@ -3,11 +3,7 @@ package fr.univ_amu.iut.exercice2;
 public class FizzBuzzer {
 
     public String computeString(int i) {
-        if (i == 1)
-            return "1";
-        if (i == 2)
-            return "2";
-        throw new RuntimeException("Not yet implemented !");
+        return String.valueOf(i);
     }
 
     public String[] computeList(int i) {
