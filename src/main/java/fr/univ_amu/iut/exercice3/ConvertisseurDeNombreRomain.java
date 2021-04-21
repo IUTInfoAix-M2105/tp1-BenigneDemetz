f211af08e3be52b6c0074f9477cf0146f39ebd7b
BenigneDe <benigne.demetz@etu.univ-amu.fr>
2021-04-21 16:46:33 +0200

    ConvertisseurDeNombreRomain implementation de X -> 10

--- src/main/java/fr/univ_amu/iut/exercice3/ConvertisseurDeNombreRomain.java
+++ src/main/java/fr/univ_amu/iut/exercice3/ConvertisseurDeNombreRomain.java
@@ -2,8 +2,9 @@ package fr.univ_amu.iut.exercice3;
 
 public class ConvertisseurDeNombreRomain {
     public int enNombreArabe(String i) {
-        if (i == "I") return 1;
-        if (i == "V") return 5;
+        if (i.equals("I")) return 1;
+        if (i.equals("V")) return 5;
+        if (i.equals("X")) return 10;
         throw new RuntimeException("Not yet implemented !");
     }
 }
