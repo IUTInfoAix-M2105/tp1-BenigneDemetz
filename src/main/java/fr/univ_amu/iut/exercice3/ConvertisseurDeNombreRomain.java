1d2f2249a7350a59e74d8f3c679612035882f5d5
BenigneDe <benigne.demetz@etu.univ-amu.fr>
2021-04-21 16:42:35 +0200

    ConvertisseurDeNombreRomain implementation de I -> 1

--- src/main/java/fr/univ_amu/iut/exercice3/ConvertisseurDeNombreRomain.java
+++ src/main/java/fr/univ_amu/iut/exercice3/ConvertisseurDeNombreRomain.java
@@ -2,6 +2,7 @@ package fr.univ_amu.iut.exercice3;
 
 public class ConvertisseurDeNombreRomain {
     public int enNombreArabe(String i) {
+        if (i == "I") return 1;
         throw new RuntimeException("Not yet implemented !");
     }
 }
